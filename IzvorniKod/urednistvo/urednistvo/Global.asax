fdf4467e7cc21bf78cfdb2944c7fbee7b84b3b21
Bruno Blažeka <bruno.blazeka@fer.hr>
2016-10-17 16:05:36 +0200

    initial commit

---
+++ IzvorniKod/urednistvo/urednistvo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="urednistvo.MvcApplication" Language="C#" %>
---
+++ IzvorniKod/urednistvo/urednistvo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(urednistvo.Startup))]
+namespace urednistvo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
