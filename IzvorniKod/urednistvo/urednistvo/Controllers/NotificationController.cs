0679b83237e414661083097d4044b8661ad94c6c
mateo <mateo@MATEO-PC>
2016-12-23 14:47:36 +0100

    Pocetak rada s NotificationControlerom (1)

---
+++ IzvorniKod/urednistvo/urednistvo/Controllers/NotificationController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using urednistvo.Models;
+
+namespace urednistvo.Controllers
+{
+    public class NotificationController : Controller
+    {
+        private UrednistvoDatabase db = new UrednistvoDatabase();
+
+        // GET: Notification
+        public ActionResult Index()
+        {
+            return View(db.Notifications.ToList());
+        }
+
+        // GET: Notification/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Notification/Create
+        public ActionResult Create()
+        {
+            return Content("Created");
+        }
+
+        // POST: Notification/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Notification/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Notification/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Notification/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Notification/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
