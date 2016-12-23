8032e3f10448bf47deac627ae614053cdc3b70ea
mateo <mateo@MATEO-PC>
2016-12-23 16:35:33 +0100

    Napravljen UserController (jedino ne radi za krivi login - exception). Potrebno dodati logout, plus prilagoditi ispis podataka (npr. Uloge)

--- IzvorniKod/urednistvo/urednistvo/Models/User.cs
+++ IzvorniKod/urednistvo/urednistvo/Models/User.cs
@@ -5,23 +5,11 @@ namespace urednistvo.Models
 {
     public class User
     {
-        [Key]
         public int UserId { get; set; }
-
-        [Required(ErrorMessage ="OJ")]
         public string FirstName { get; set; }
-
-        [Required(ErrorMessage = "OJ")]
         public string LastName { get; set; }
-
-        [Required(ErrorMessage = "OJ")]
         public string UserName { get; set; }
-
-        [Required(ErrorMessage = "OJ")]
         public string Email { get; set; }
-
-        [Required(ErrorMessage = "OJ")]
-        [DataType(DataType.Password)]
         public string Password { get; set; }
         public int Role { get; set; } //enum
 
