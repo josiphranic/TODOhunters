5d3fa349f037fdced063b6c7b3cb06782c028ba6
Lukrecija <Lukrecija@DESKTOP-M7FP4EA>
2017-01-03 18:05:25 +0100

    ...

--- IzvorniKod/urednistvo/urednistvo/Controllers/StatisticsController.cs
+++ IzvorniKod/urednistvo/urednistvo/Controllers/StatisticsController.cs
@@ -96,7 +96,6 @@ namespace urednistvo.Controllers
                 }
                 authors.Add(aView);
             }
-            
             return View(authors);
         }
 
@@ -114,27 +113,6 @@ namespace urednistvo.Controllers
             aView.numSentTexes = 0;
 
             return aView;
-        }
-
-        private List<TextView> addTexts(List<Text> texts)
-        {
-            List<TextView> tView = new List<TextView>();
-
-            foreach(Text t in texts)
-            {
-                tView.Add(TextController.getTextView(t));
-            }
-            return tView;
-        }
-
-        private List<EditionView> addEditions (List<Edition> editions)
-        {
-            List<EditionView> eView = new List<EditionView>();
-            foreach(Edition e in editions)
-            {
-                eView.Add(EditionsController.createEditionView(e));
-            }
-            return eView;
         }
 
         private List<User> getAuthors()
