ea54203c18bd8381d7489a36bf17a150c354d06f
Lukrecija <Lukrecija@DESKTOP-M7FP4EA>
2017-01-04 13:11:39 +0100

    Statistics

--- IzvorniKod/urednistvo/urednistvo/Controllers/StatisticsController.cs
+++ IzvorniKod/urednistvo/urednistvo/Controllers/StatisticsController.cs
@@ -15,7 +15,7 @@ namespace urednistvo.Controllers
         // GET: Statistics
         public ActionResult Index()
         {
-            if ((String)Session["Role"] != RoleNames.EDITOR || (String)Session["Role"] != RoleNames.EDITORIAL_COUNCIL_MEMBER)
+            if (!((String)Session["Role"] == RoleNames.EDITOR || (String)Session["Role"] == RoleNames.EDITORIAL_COUNCIL_MEMBER))
             {
                 TempData["Message"] = "Samo glavni urednik i članovi uredničko vijeća imaju pristup statistikama.";
                 return RedirectToAction("Index", "Statistics");
